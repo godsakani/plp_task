c423b29ad8fa18dc3582350dc5171235d9a7e330
godsakani <nyonggodwill11@gmail.com>
2025-08-25 16:39:41 +0200

    completed week 2

---
+++ week2.py
@@ -0,0 +1,13 @@
+#Task 1:
+my_list = []
+#Task 2:
+my_list=[10,20,30,40]
+#Task 3:
+my_list[1]=15
+#Task 4:
+my_list.extend([50,60,70])
+#Task 5:
+my_list.sort(reverse=False)
+#Task 6:
+print(my_list[2])
+print(my_list)
